50d49f1f0962d14e32eea7dd836c216f79d35fd0
Zainul Abideen <waeezazeem@gmail.com>
2023-06-02 01:34:07 +0530

    check_odd_even

---
+++ odd_even.js
@@ -0,0 +1,11 @@
+// n9um % 2 === 0;
+
+const a = [1, 2, 4, 2, 0, 3, 10, 55, 10];
+for (let i = 0; i < 9; i++){
+    if( a[i] % 2 === 0){
+        console.log("even", a[i])
+    }
+    else{
+        console.log("odd", a[i])
+    }
+}
