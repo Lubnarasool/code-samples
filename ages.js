cbb714ad2882d202518c6dc6064748313be6e907
Lubnarasool <135058180+Lubnarasool@users.noreply.github.com>
2023-06-13 01:55:54 +0530

    Create ages.js

---
+++ ages.js
@@ -0,0 +1,9 @@
+const ages = [10, 18, 30, 15, 5, 55, 11, 70, 40, 22, 27, 20, 4];
+for (let i = 0; i < 13; i++) {
+    if (ages[i] !== 55) {
+        console.log(ages[i]);
+    }
+    else {
+        console.log("hello")
+    }
+}
