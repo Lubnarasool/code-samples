507cfd6bdbb2c880d1017dcd3427d0ff3cb94e0c
Zainul Abideen <waeezazeem@gmail.com>
2023-06-28 00:25:16 +0530

    CtoF- function

---
+++ CtoF-function.js
@@ -0,0 +1,10 @@
+function temp(c){
+    let f = c*9/5+32
+    return f
+}
+
+
+console.log(temp(56))
+
+const te = temp(32);
+console.log(te)
--- arrAvg(.js
+++ arrAvg(.js
@@ -1,5 +1,5 @@
 function arrAvg(a){
-    sum = 0;
+    let sum = 0;
     for (let i = 0; i < a.length; i++){
         sum = sum+a[i]
     }
