93fa46e40cad98c2bbf2ee8d569fe9c4b845dd09
Zainul Abideen <waeezazeem@gmail.com>
2023-06-28 23:58:01 +0530

    functionPractice

--- disfunction.js
+++ disfunction.js
@@ -1,6 +1,6 @@
 function calculateDiscount(price, perc){
-    let disAmt = price*perc/100;
-    let finalAmt = price-disAmt;
+    let disAmt = price*perc/100; //9000*5/100 = 450
+    let finalAmt = price-disAmt; //9000-450 =8550
     return finalAmt
 }
  
@@ -8,6 +8,16 @@ function calculateDiscount(price, perc){
 
 const prices = [9000, 200, 800, 3000, 10000, 7600];
 for (let i = 0; i < prices.length; i++){
-    const finalAmount = calculateDiscount(prices[i],5);
-    console.log(finalAmount)
+    // const finalAmount = calculateDiscount(prices[i],5);
+    //   console.log(finalAmount)
+      if(prices[i]>5000){
+        const finalAmount = calculateDiscount(prices[i],10);
+        console.log(finalAmount)
+      }
+
+      else{
+        const finalAmount = calculateDiscount(prices[i],5);
+        console.log(finalAmount)
+      }
+
 }
