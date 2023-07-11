a8c490b91154a20a2544c728b329ef635e13524d
Zainul Abideen <waeezazeem@gmail.com>
2023-07-11 17:35:55 +0530

    footToInch.js

---
+++ footToInch.js
@@ -0,0 +1,9 @@
+// Write a function that accepts a parameter of type integer as foot and converts the the number into inches.
+// inches = foot * 12
+function footToInch(a){
+    let inch = a*12
+
+    return inch
+}
+footToInch(1)
+console.log(footToInch(1))
