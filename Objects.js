2297c9f40db3fa599a1d6c2e2acc007fa2714c19
Zainul Abideen <waeezazeem@gmail.com>
2023-07-22 00:52:27 +0530

    oblects and array of objects

---
+++ Objects.js
@@ -0,0 +1,38 @@
+// const username = "lubna rasool"
+// const dob = 1996
+// const email = "lubnarasool0001@gmail.com"
+// const passcode = "ZAINUBNA"
+// const followers = [
+//     "zainul", 
+//     "samaira", 
+//     "moonis"
+// ];
+// const isActive = true;
+const todaysDate = "20/07/2023"
+
+const user = {
+    username: "lubna rasool",
+    dob: 1996,
+    email: "lubnarasool0001@gmail.com",
+    passcode: "ZAINUBNA",
+    followers: [ "zainul", "samaira", "moonis" ],
+    isActive: true
+};
+
+// if(user.dob > 1997){
+
+// }
+
+console.log(user) // prints all data in an object
+console.log(user.email); // prints single value from an object
+const plusOne = user.dob + 1
+console.log(plusOne)
+
+
+
+const car = {
+    color: "red",
+    brand: "hyundai",
+    model: "i20"
+};
+console.log(car.brand)
---
+++ arrayofObjects.js
@@ -0,0 +1,31 @@
+const users = [{
+    username: "lubna rasool",
+    dob: 1996,
+    email: "lubnarasool0001@gmail.com",
+    passcode: "ZAINUBNA",
+    followers: [ "zainul", "samaira", "moonis" ],
+    isActive: true
+}, {
+    username: "Zaikn rasool",
+    dob: 1996,
+    email: "zain@gmail.com",
+    passcode: "123",
+    followers: [ "zainul", "samaira", "moonis" ],
+    isActive: false
+}];
+console.log(users[1].email);
+
+const cars = [{
+    color: "red",
+    brand: "hyundai",
+    model: "i20"
+},{
+    color: "pink",
+    brand: "mahendra",
+    model: "XUV"
+},{
+    color: "Silver",
+    brand: "kia",
+    model: "seltos"
+}];
+    console.log(cars[2].model)
